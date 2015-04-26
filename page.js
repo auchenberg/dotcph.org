bf25d16ff65f8cf2294256e45699d179dcfdc6a7
Kenneth Auchenberg <kenneth@auchenberg.dk>
2015-04-26 23:12:10 +0630

    Fancy typing effect

---
+++ page.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('.typing').typed({
+    strings: [
+      'dot<strong>cph</strong>', 
+      'coffee<strong>cph</strong>', 
+      'fuck<strong>cph</strong>', 
+      'city<strong>cph</strong>', 
+      'our<strong>cph</strong>', 
+      'weather<strong>cph</strong>', 
+      'unite<strong>cph</strong>', 
+      'music<strong>cph</strong>', 
+      'be<strong>cph</strong>',
+      'live<strong>cph</strong>',
+      'bikes<strong>cph</strong>',
+      'life<strong>cph</strong>',
+      'food<strong>cph</strong>',
+      'nature<strong>cph</strong>',
+      'festival<strong>cph</strong>',
+    ],
+    typeSpeed: 150,
+    backDelay: 300,
+    backSpeed: 300,
+    loop: true
+  });
+}); 
